a2dfbc0fb035bd1532bcac0667e54ab67e4d06cb
Caitlin Lindauer <clilos018@gmail.com>
2022-04-29 14:33:16 -0600

    added app.use

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
